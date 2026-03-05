f2eeb5f21c8fe6c94e799974b19707f9ad9486c8
caixj <181374379@qq.com>
2026-03-05 11:14:10 +0800

    feat 初始化仓库

---
+++ src/yuwen/grade1/part-one/path.ts
@@ -0,0 +1,173 @@
+/**
+ * 课程目录列表
+ */
+export const lessons:Record<string,any>[] = [
+  {
+    "id": 1,
+    "number": "1",
+    "title": "古诗三首",
+    "author": "杜甫、白居易、王安石"
+  },
+  {
+    "id": 2,
+    "number": "2",
+    "title": "燕子",
+    "author": "郑振铎"
+  },
+  {
+    "id": 3,
+    "number": "3",
+    "title": "荷花",
+    "author": "叶圣陶"
+  },
+  {
+    "id": 4,
+    "number": "4",
+    "title": "昆虫备忘录",
+    "author": "汪曾祺"
+  },
+  {
+    "id": 5,
+    "number": "5",
+    "title": "守株待兔",
+    "author": "韩非子"
+  },
+  {
+    "id": 6,
+    "number": "6",
+    "title": "陶罐和铁罐",
+    "author": "黄瑞云"
+  },
+  {
+    "id": 7,
+    "number": "7",
+    "title": "狮子和鹿",
+    "author": "伊索寓言"
+  },
+  {
+    "id": 8,
+    "number": "8",
+    "title": "池子与河流",
+    "author": "克雷洛夫"
+  },
+  {
+    "id": 9,
+    "number": "9",
+    "title": "古诗三首",
+    "author": "王安石、杜甫、韦应物"
+  },
+  {
+    "id": 10,
+    "number": "10",
+    "title": "纸的发明",
+    "author": null
+  },
+  {
+    "id": 11,
+    "number": "11",
+    "title": "赵州桥",
+    "author": null
+  },
+  {
+    "id": 12,
+    "number": "12",
+    "title": "一幅名扬中外的画",
+    "author": null
+  },
+  {
+    "id": 13,
+    "number": "13",
+    "title": "花钟",
+    "author": null
+  },
+  {
+    "id": 14,
+    "number": "14",
+    "title": "蜜蜂",
+    "author": "法布尔"
+  },
+  {
+    "id": 15,
+    "number": "15",
+    "title": "小虾",
+    "author": "菁莽"
+  },
+  {
+    "id": 16,
+    "number": "16",
+    "title": "宇宙的另一边",
+    "author": "陈诗哥"
+  },
+  {
+    "id": 17,
+    "number": "17",
+    "title": "我变成了一棵树",
+    "author": "顾鹰"
+  },
+  {
+    "id": 18,
+    "number": "18",
+    "title": "童年的水墨画",
+    "author": "张继楼"
+  },
+  {
+    "id": 19,
+    "number": "19",
+    "title": "剃头大师",
+    "author": "秦文君"
+  },
+  {
+    "id": 20,
+    "number": "20",
+    "title": "肥皂泡",
+    "author": "冰心"
+  },
+  {
+    "id": 21,
+    "number": "21",
+    "title": "我不能失信",
+    "author": "孙永猛"
+  },
+  {
+    "id": 22,
+    "number": "22",
+    "title": "我们奇妙的世界",
+    "author": "彼得·西摩"
+  },
+  {
+    "id": 23,
+    "number": "23",
+    "title": "海底世界",
+    "author": null
+  },
+  {
+    "id": 24,
+    "number": "24",
+    "title": "火烧云",
+    "author": "萧红"
+  },
+  {
+    "id": 25,
+    "number": "25",
+    "title": "慢性子裁缝和急性子顾客",
+    "author": "周锐"
+  },
+  {
+    "id": 26,
+    "number": "26",
+    "title": "方帽子店",
+    "author": "施雁冰"
+  },
+  {
+    "id": 27,
+    "number": "27",
+    "title": "漏",
+    "author": null
+  },
+  {
+    "id": 28,
+    "number": "28",
+    "title": "枣核",
+    "author": null
+  }
+]
